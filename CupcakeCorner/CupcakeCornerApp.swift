5958db66aaa59698964e93cfc6ad0ab3c18daf5f
Beto Toro <beto.toro@zemoga.com>
2022-07-21 16:33:11 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Beto Toro on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
